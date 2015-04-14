2bc540875f0692e312fc01c9dd3525b3821b66de
unknown <sephirot47@PINKY.vgasmb.vgafib.com>
2015-04-14 11:48:37 +0200

    Blood added

--- Assets/Baby.cs
+++ Assets/Baby.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 
 public class Baby : MonoBehaviour 
 {
+	int jumps = 0;
+
 	private static int numArticulations = 4;
-	private float rotSpeed = 30.0f, forwardSpeed = 0.1f, sideSpeed = 0.1f, jumpForce = 12.0f;
+	private float rotSpeed = 10.0f, forwardSpeed = 0.1f, sideSpeed = 0.1f, jumpForce = 12.0f;
+
+	public float bloodiness = 0.5f;
 
 	private Quaternion originalHipLRotation, originalHipRRotation; 
 	private Transform initialTransform;
-
+	
 	public KeyCode forwardKey, backwardKey, leftKey, rightKey, jumpKey, resetKey; 
 
 	private class Articulations
@@ -75,14 +79,35 @@ public class Baby : MonoBehaviour
 
 			if(Input.GetKey(forwardKey)) GetComponent<Rigidbody>().AddTorque(new Vector3(1,0,0) * sideSpeed, ForceMode.Impulse);
 			else if(Input.GetKey(backwardKey)) GetComponent<Rigidbody>().AddTorque(new Vector3(-1,0,0) * sideSpeed, ForceMode.Impulse);
+
+			if( Input.GetKey(forwardKey) ) GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1) * forwardSpeed, ForceMode.VelocityChange);
+			if( Input.GetKey(backwardKey) ) GetComponent<Rigidbody>().AddForce(new Vector3(0,0,-1) * forwardSpeed, ForceMode.VelocityChange);
 		}
 		
-		if( Input.GetKeyDown(jumpKey) && !Jumping())
+		if( Input.GetKeyDown(jumpKey) && jumps <= 1)
 		{
+			++jumps;
 			GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
 		}
 
 		if( Input.GetKey(leftKey) ) GetComponent<Rigidbody>().AddTorque(new Vector3(0,0,1), ForceMode.Impulse);
 		if( Input.GetKey(rightKey) ) GetComponent<Rigidbody>().AddTorque(new Vector3(0,0,-1), ForceMode.Impulse);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if(col.collider.GetComponent<Transform>().gameObject.name.Contains("Floor"))
+		{
+			Vector3 hitPoint = col.contacts[0].point;
+
+			if(Mathf.Abs(GetComponent<Rigidbody>().velocity.x) >= 1.0f/bloodiness || Mathf.Abs(GetComponent<Rigidbody>().velocity.y) >= 1.0f/bloodiness)
+			{
+				GameObject go = (GameObject)Instantiate(Resources.Load("BloodQuad"), hitPoint + col.contacts[0].normal * 0.001f , Quaternion.LookRotation(-col.contacts[0].normal));
+				float a = Mathf.Max(1.0f, bloodiness * GetComponent<Rigidbody>().velocity.magnitude) * 0.5f;
+				go.transform.localScale = new Vector3(a,a,a);
+			}
+
+			jumps = 0;
+		}
 	}
 }
